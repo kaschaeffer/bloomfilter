cab95954c1efcd2a351fa28e3bf71670838c7c52
kaschaeffer <kevin.a.schaeffer@gmail.com>
2014-04-27 18:12:42 -0700

    go fmt'ed files

--- bloomfilter.go
+++ bloomfilter.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "crypto/sha256"
-    "encoding/binary"
-    "bytes"
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
 )
 
 // constructor
 func NewBloomFilterStringKeyed(byteCapacity int, numHashes int) *BloomFilterStringKeyed {
-    bloomFilter := new(BloomFilterStringKeyed)
-    bloomFilter.byteCapacity = byteCapacity
-    bloomFilter.numHashes = numHashes
-    bloomFilter.bitHashTable = make([]byte, byteCapacity)
-    return bloomFilter   
+	bloomFilter := new(BloomFilterStringKeyed)
+	bloomFilter.byteCapacity = byteCapacity
+	bloomFilter.numHashes = numHashes
+	bloomFilter.bitHashTable = make([]byte, byteCapacity)
+	return bloomFilter
 }
 
 type BloomFilterStringKeyed struct {
-    byteCapacity, numHashes int
-    bitHashTable []byte
+	byteCapacity, numHashes int
+	bitHashTable            []byte
 }
 
 // IDEA: maybe pass in which hashing algorithm you want in the constructor
@@ -28,15 +28,14 @@ type BloomFilterStringKeyed struct {
 // for the hash functions...
 
 func (b *BloomFilterStringKeyed) AddKey(key string) {
-    // get hashes of the key
-    hashes := b.generateHashesFromString(key)
-    indices := b.convertHashesToCorrectRange(hashes)
-    fmt.Println(indices)
+	// get hashes of the key
+	hashes := b.generateHashesFromString(key)
+	indices := b.convertHashesToCorrectRange(hashes)
+	fmt.Println(indices)
 
-    // flip bits in the hash table (which will be represented as an array of bytes)
+	// flip bits in the hash table (which will be represented as an array of bytes)
 
-
-    // do something with the key...
+	// do something with the key...
 }
 
 // func (b &BloomFilterStringKeyed) setBitsFromIndices([]uint64 indices) {
@@ -53,55 +52,55 @@ func (b *BloomFilterStringKeyed) AddKey(key string) {
 
 func (b *BloomFilterStringKeyed) convertHashesToCorrectRange(hashes [][]byte) []uint64 {
 
-    // TODO first should check the length of the array 
+	// TODO first should check the length of the array
 
-    indices := make([]uint64, b.numHashes)
+	indices := make([]uint64, b.numHashes)
 
-    for i:=0; i<b.numHashes; i++ {    
-        hashAsInt := convertByteArraytoUInt64(hashes[i])
-        indices[i] = hashAsInt % uint64(b.byteCapacity)
-    }
+	for i := 0; i < b.numHashes; i++ {
+		hashAsInt := convertByteArraytoUInt64(hashes[i])
+		indices[i] = hashAsInt % uint64(b.byteCapacity)
+	}
 
-    return indices
+	return indices
 }
 
 func convertByteArraytoUInt64(byteArray []byte) uint64 {
-    convertedByteArray := binary.LittleEndian.Uint64(byteArray)
-    return convertedByteArray
+	convertedByteArray := binary.LittleEndian.Uint64(byteArray)
+	return convertedByteArray
 }
 
 func (b *BloomFilterStringKeyed) generateHashesFromString(key string) [][]byte {
-    // get a 32-byte hash of the key
-    hashedKey := HashString(key)
-    fmt.Println(hashedKey)
+	// get a 32-byte hash of the key
+	hashedKey := HashString(key)
+	fmt.Println(hashedKey)
 
-    // call this "seedHash" instead?
+	// call this "seedHash" instead?
 
-    // split into two 8-byte hashes
-    // TODO: don't hardcode 8 below..
-    hashedKey0 := hashedKey[:8]
-    hashedKey1 := hashedKey[8:16]
+	// split into two 8-byte hashes
+	// TODO: don't hardcode 8 below..
+	hashedKey0 := hashedKey[:8]
+	hashedKey1 := hashedKey[8:16]
 
-    // max filter size should be 2^(16*8)
+	// max filter size should be 2^(16*8)
 
-    fmt.Printf("hashedKey0 = %d\n", hashedKey0)
-    fmt.Printf("hashedKey1 = %d\n", hashedKey1)
+	fmt.Printf("hashedKey0 = %d\n", hashedKey0)
+	fmt.Printf("hashedKey1 = %d\n", hashedKey1)
 
-    // list of hashes
-    fmt.Printf("numHashes = %d\n", b.numHashes)
-    hashes := make([][]byte, b.numHashes)
+	// list of hashes
+	fmt.Printf("numHashes = %d\n", b.numHashes)
+	hashes := make([][]byte, b.numHashes)
 
-    for k:=0; k<b.numHashes; k++ {
-        kByte := byte(k)
+	for k := 0; k < b.numHashes; k++ {
+		kByte := byte(k)
 
-        hashes[k] = make([]byte, 8)
-        for i:=0; i<len(hashedKey0); i++ {
-            // hashes[k][i] = hashedKey0[i] + (hashedKey1[i]*byte(k))
-            hashes[k][i] = hashedKey0[i] ^ (hashedKey1[i]*kByte)
+		hashes[k] = make([]byte, 8)
+		for i := 0; i < len(hashedKey0); i++ {
+			// hashes[k][i] = hashedKey0[i] + (hashedKey1[i]*byte(k))
+			hashes[k][i] = hashedKey0[i] ^ (hashedKey1[i] * kByte)
 
-        }
-    }
-    return hashes
+		}
+	}
+	return hashes
 }
 
 // func byteMultiplication(b byte, k int) {
@@ -119,68 +118,66 @@ func (b *BloomFilterStringKeyed) generateHashesFromString(key string) [][]byte {
 // **should be able to do this via bit-shifting tricks**
 
 func setBitInByte(b *byte, bitPosition uint) (err error) {
-    if (bitPosition>7) {
-        err = fmt.Errorf(
-            "Position of bit to set must be in the range [0,7), attempted to set position %", bitPosition)
-    }
-    *b = *b | (1 << bitPosition)
-    return
+	if bitPosition > 7 {
+		err = fmt.Errorf(
+			"Position of bit to set must be in the range [0,7), attempted to set position %", bitPosition)
+	}
+	*b = *b | (1 << bitPosition)
+	return
 }
 
 /////////////////////////////////////////////////
 //       general purpose hashing functions     //
 /////////////////////////////////////////////////
 
-func HashString(key string)([32]byte) {
-    byteKey := StringToByteArray(key)
-    hashedKey := HashByteArray(byteKey)
-    return hashedKey
+func HashString(key string) [32]byte {
+	byteKey := StringToByteArray(key)
+	hashedKey := HashByteArray(byteKey)
+	return hashedKey
 }
 
-func StringToByteArray(str string)([]byte) {
-    return []byte(str)
+func StringToByteArray(str string) []byte {
+	return []byte(str)
 }
 
 // general function that will be used for all hashing
-func HashByteArray(byteArray []byte)([32]byte) {
-    hash := sha256.Sum256(byteArray)   
-    return hash 
+func HashByteArray(byteArray []byte) [32]byte {
+	hash := sha256.Sum256(byteArray)
+	return hash
 }
-
 
 // Testing out how to hash some object
 func main() {
-    fmt.Println("Hello, playground")
-    
-    // sha := sha256.New()
-    
-    result := sha256.Sum256([]byte("foobar"))
-    length := len(result)
-    fmt.Printf("%d\n", length)
-    fmt.Printf("%x\n", result)
+	fmt.Println("Hello, playground")
 
-    part0 := result[:16]
-    fmt.Printf("%d\n", len(part0))
-    fmt.Printf("%x\n", part0)
-    
-    // converting byte array to signed int
-    buf := bytes.NewBuffer(part0)
-    intPart0, _ := binary.ReadVarint(buf)
-    // note: probably want unsigned int instead...
+	// sha := sha256.New()
 
-    fmt.Printf("%d\n\n\n", intPart0)
+	result := sha256.Sum256([]byte("foobar"))
+	length := len(result)
+	fmt.Printf("%d\n", length)
+	fmt.Printf("%x\n", result)
 
+	part0 := result[:16]
+	fmt.Printf("%d\n", len(part0))
+	fmt.Printf("%x\n", part0)
 
-    part1 := result[16:]
-    fmt.Printf("%d\n", len(part1))
-    fmt.Printf("%x\n", part1) 
+	// converting byte array to signed int
+	buf := bytes.NewBuffer(part0)
+	intPart0, _ := binary.ReadVarint(buf)
+	// note: probably want unsigned int instead...
 
-    b := NewBloomFilterStringKeyed(32, 10)
+	fmt.Printf("%d\n\n\n", intPart0)
 
-    fmt.Println(b.byteCapacity)
-    fmt.Println(b.numHashes)
-    fmt.Println(b.bitHashTable)
+	part1 := result[16:]
+	fmt.Printf("%d\n", len(part1))
+	fmt.Printf("%x\n", part1)
 
-    b.AddKey("foo")
+	b := NewBloomFilterStringKeyed(32, 10)
+
+	fmt.Println(b.byteCapacity)
+	fmt.Println(b.numHashes)
+	fmt.Println(b.bitHashTable)
+
+	b.AddKey("foo")
 
 }
--- bloomfilter_test.go
+++ bloomfilter_test.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 var setBitInByteTestValues = []struct {
-    in byte
-    inWhichBit uint
-    out byte // TODO: is the variable name 'out' confusing here since SetbitInByte is in-place?
+	in         byte
+	inWhichBit uint
+	out        byte // TODO: is the variable name 'out' confusing here since SetbitInByte is in-place?
 }{
-    {0x00, 0, 0x01},
-    {0x00, 1, 0x02},
-    {0x00, 2, 0x04},
-    {0x00, 3, 0x08},
-    {0x00, 4, 0x10},
-    {0x00, 5, 0x20},
-    {0x00, 6, 0x40},
-    {0x00, 7, 0x80},
-    {0x01, 0, 0x01}, // setting a bit that's already 1 should have no effect
-    {0x01, 1, 0x03},
-    {0x03, 1, 0x03},
+	{0x00, 0, 0x01},
+	{0x00, 1, 0x02},
+	{0x00, 2, 0x04},
+	{0x00, 3, 0x08},
+	{0x00, 4, 0x10},
+	{0x00, 5, 0x20},
+	{0x00, 6, 0x40},
+	{0x00, 7, 0x80},
+	{0x01, 0, 0x01}, // setting a bit that's already 1 should have no effect
+	{0x01, 1, 0x03},
+	{0x03, 1, 0x03},
 }
 
 func TestSetBitInByte(t *testing.T) {
-    for i:=0; i<len(setBitInByteTestValues); i++ {
-        testValues := setBitInByteTestValues[i]
+	for i := 0; i < len(setBitInByteTestValues); i++ {
+		testValues := setBitInByteTestValues[i]
 
-        in := testValues.in // make a copy since setByteInBit mutates testValues.in
-        if setBitInByte(&testValues.in, testValues.inWhichBit); testValues.in != testValues.out {
-            t.Errorf("setByteInBit(%#02x, %d) = %#02x, want %#02x", in, testValues.inWhichBit, testValues.in, testValues.out)
-        }
-    }
+		in := testValues.in // make a copy since setByteInBit mutates testValues.in
+		if setBitInByte(&testValues.in, testValues.inWhichBit); testValues.in != testValues.out {
+			t.Errorf("setByteInBit(%#02x, %d) = %#02x, want %#02x", in, testValues.inWhichBit, testValues.in, testValues.out)
+		}
+	}
 }
 
 func TestSetBitInByteIncorrectBitPosition(t *testing.T) {
-    var bitPosition uint
-    var in byte
-    
-    bitPosition = 9
-    in = 0x00
-    err := setBitInByte(&in, bitPosition)
-    
-    if err == nil {
-        t.Errorf("setByteInBit(%#02x, %d) should return error, but did not", in, bitPosition)
-    }
+	var bitPosition uint
+	var in byte
+
+	bitPosition = 9
+	in = 0x00
+	err := setBitInByte(&in, bitPosition)
+
+	if err == nil {
+		t.Errorf("setByteInBit(%#02x, %d) should return error, but did not", in, bitPosition)
+	}
 }
